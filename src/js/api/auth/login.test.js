b028b1d7ec1182f5e5a8064c72f8d7ca2ce82096
Benjamin Meldal <benjamin.meldal@gmail.com>
2023-04-10 15:53:45 +0200

    some clean-up in my test files

--- src/js/api/auth/login.test.js
+++ src/js/api/auth/login.test.js
@@ -1,11 +1,11 @@
-import { mockLocalStorage } from "../../storage/mockStorage";
-import { login } from "./login";
+import { mockLocalStorage } from '../../storage/mockStorage';
+import { login } from './login';
 
-const CREDENTIALS = { email: "benjatest@stud.noroff.no", password: "test1234" };
-const ACCESSTOKEN = "123abc";
+const ACCESSTOKEN = '123abc';
 const PROFILE = {
-  name: "benjamel",
-  email: "benjatest@stud.noroff.no",
+  name: 'benjatesting',
+  email: 'benjatesting@noroff.no',
+  password: 'testing1234',
   accessToken: ACCESSTOKEN,
 };
 
@@ -15,18 +15,18 @@ function validCredentials() {
   return Promise.resolve({
     ok: true,
     status: 200,
-    statusText: "OK",
+    statusText: 'OK',
     json: jest.fn().mockResolvedValue(PROFILE),
   });
 }
 
-describe("login", () => {
-  it("logs in the user with email and password, and stores a token in localstorage", async () => {
+describe('login', () => {
+  it('logs in the user with email and password, and stores a token in localstorage', async () => {
     global.localStorage.clear();
     global.fetch = jest.fn(() => validCredentials());
-    await login(CREDENTIALS.email, CREDENTIALS.password);
+    await login(PROFILE.email, PROFILE.password);
 
-    expect(JSON.parse(global.localStorage.getItem("token"))).toBe(ACCESSTOKEN);
-    expect(JSON.parse(global.localStorage.getItem("profile"))).toEqual(PROFILE);
+    expect(JSON.parse(global.localStorage.getItem('token'))).toBe(ACCESSTOKEN);
+    expect(JSON.parse(global.localStorage.getItem('profile'))).toEqual(PROFILE);
   });
 });
